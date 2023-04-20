cb7cfd295d4d02de57fb767bb59664f9fd0a8a89
margaritafisher <mfisherjantus@itba.edu.ar>
2023-04-20 18:27:28 -0300

    validaciones arquero

--- Arquero.py
+++ Arquero.py
@@ -1,11 +1,10 @@
 import Jugador
 
 class Arquero(Jugador):
-    def __init__(self, nombre, apellido, dni, fechanacimiento, nacionalidad, estatura, peso, valor, club, estado, cantidad_partidos, vallas_invictas, goles_recibidos, penales_atajados):
+    def __init__(self, nombre, apellido, dni, fechanacimiento, nacionalidad, estatura, peso, valor, club, estado, cantidad_partidos, vallas_invictas, goles_recibidos):
         Jugador.__init__(self, nombre, apellido, dni, fechanacimiento, nacionalidad, estatura, peso, valor, club, estado, cantidad_partidos)
         self.vallas_invictas = vallas_invictas
         self.goles_recibidos = goles_recibidos
-        self.penales_atajados = penales_atajados
 
     def AtajarPenal(self):
         self.penales_atajados += 1
@@ -15,4 +14,14 @@ class Arquero(Jugador):
 
     def RecibirGol(self):
         self.goles_recibidos += 1
+
+    def validar_vallas_invictas(self):
+        while self.vallas_invictas < 0:
+            self.vallas_invictas = int(input("Ingrese un numero valido de vallas invictas: "))
+        return self.vallas_invictas
+    
+    def validar_goles_recibidos(self):
+        while self.goles_recibidos < 0:
+            self.goles_recibidos = int(input("Ingrese una cantidad valida de goles recibidos: "))
+        return self.goles_recibidos
         
