f5782f8b61c0dd18a109a1ea57c0a323cbef6551
juanmaibarra <jibarra@itba.edu.ar>
2023-05-01 18:31:27 -0300

    update str

--- JugadorDeCampo.py
+++ JugadorDeCampo.py
@@ -7,8 +7,8 @@ class JugadorDeCampo(Jugador):
         self.asistencia = asistencia
 
     def __str__(self):
-        cadena = "El nombre del jugador es {} {}, cuyo DNI es {}, nacio en la fecha {}, y cuya nacionalidad es {}. Su estatura es {} metros, su peso es {} kg. Su valor es {}, pertenece al club {}. Su estado actual es {}, tiene {} partidos jugados, hizo {} goles, y tiene {} asistencias. ".format(self.nombre, self.apellido, self.dni, self.fecha_nacimiento, self.nacionalidad, self.estatura, self.peso, self.valor, self.club, self.estado, self.cantidad_partidos, self.goles, self.asistencia)
-        return cadena 
+        return("El nombre del jugador es {} {}, cuyo DNI es {}, nacio en la fecha {}, y cuya nacionalidad es {}. Su estatura es {} metros, su peso es {} kg. Su valor es {}, pertenece al club {}. Su estado actual es {}, tiene {} partidos jugados, hizo {} goles, y tiene {} asistencias. ").format(self.nombre, self.apellido, self.dni, self.fecha_nacimiento, self.nacionalidad, self.estatura, self.peso, self.valor, self.club, self.estado, self.cantidad_partidos, self.goles, self.asistencia)
+         
 
     def HacerGol(self):
         self.goles += 1
