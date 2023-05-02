43b5953b1989586469980d714581b13c995c9ea7
liammacgaw <lmacgaw@itba.edu.ar>
2023-05-02 15:01:43 -0300

    9

--- CrearArchivos.py
+++ CrearArchivos.py
@@ -31,10 +31,10 @@ def guardar_archivos():
                with open('./arqueros.csv','w') as archivo_arqueros:
                     with open('./jugadorescampo.csv','w') as archivo_jugadorescampo:
                          for liga in Liga.lista_ligas:
-                              archivo_ligas.writerow(liga.nombre + ',' + liga.pais)
+                              archivo_ligas.writerow(liga.nombre + ',' + liga.pais + ',' + liga.lista_clubes + ',' + liga.cant_clubes)
 
                          for club in Club.lista_clubes:
-                              archivo_clubes.writerow(club.nombre + ',' + club.id + ',' + club.liga + ',' + club.presupuesto + ',' + club.valor_del_club)
+                              archivo_clubes.writerow(club.nombre + ',' + club.id + ',' + club.liga + ',' + club.presupuesto + ',' + club.valor_del_club + ',' + club.lista_jugadores)
 
                          for arquero in Arquero.lista_arqueros:
                               archivo_arqueros.writerow(arquero.nombre + ',' + arquero.apellido + ',' + arquero.dni + ',' + arquero.edad + ',' + arquero.nacionalidad + ',' + arquero.estatura + ',' + arquero.peso + ',' + arquero.valor + ',' + arquero.club + ',' + arquero.estado + ',' + arquero.cantidad_partidos + ',' + arquero.cantidad_tarjetas + ',' + arquero.posicion + ',' + arquero.vallas_invictas + ',' + arquero.goles_recibidos)
@@ -43,33 +43,23 @@ def guardar_archivos():
                               archivo_jugadorescampo.writerow(jugadorcampo.nombre + ',' + jugadorcampo.apellido + ',' + jugadorcampo.dni + ',' + jugadorcampo.edad + ',' + jugadorcampo.nacionalidad + ',' + jugadorcampo.estatura + ',' + jugadorcampo.peso + ',' + jugadorcampo.valor + ',' + jugadorcampo.club + ',' + jugadorcampo.estado + ',' + jugadorcampo.cantidad_partidos + ',' + jugadorcampo.cantidad_tarjetas + ',' + jugadorcampo.posicion + ',' + jugadorcampo.goles + ',' + jugadorcampo.asistencias)
 
 
-# def guardarEnArchivo(self,archivo_cuentas,archivo_depositos,archivo_retiros):
-#      archivo_cuentas.writerow(self.titular.nombre + "," + self.titular.dni + "," + self.titular.genero + "," + self.cbu)
+def cargar_archivo():
+     with open('./ligas.csv','r') as archivo_ligas:
+          with open('./clubes.csv','r') as archivo_clubes:
+               with open('./arqueros.csv','r') as archivo_arqueros:
+                    with open('./jugadorescampo.csv','r') as archivo_jugadorescampo:
+                         for liga in archivo_ligas:
+                              datos_liga = liga.split(',')
+                              Liga(datos_liga[0],datos_liga[1],datos_liga[2],datos_liga[3])
 
-#      for monto in self.depositos:
-#           archivo_depositos.writerow(self.cbu + "," + monto)
+                         for club in archivo_clubes:
+                              datos_club = club.split(',')
+                              Club(datos_club[0],datos_club[1],datos_club[2],datos_club[3],datos_club[4],datos_club[5])
 
-#      for monto in self.retiros:
-#           archivo_retiros.writerow(self.cbu + "," + monto)
-
-# def guardarPrograma():
-#      with open("./cuentas.csv","w") as archivo_cuentas:
-#           with open("./depositos.csv","w") as archivo_depositos:
-#                with open("./retiros.csv","w") as archivo_retiros:
-#                     for cuentas in Cuenta.dict_cuentas.values():
-#                          Cuenta.guardarEnArchivo(archivo_cuentas,archivo_depositos,archivo_retiros)
-
-# def cargarPrograma():
-#      with open("./cuentas.csv","r") as archivo_cuentas:
-#           with open("./depositos.csv","r") as archivo_depositos:
-#                with open("./retiros.csv","r") as archivo_retiros:
-#                     for cuenta in archivo_cuentas:
-#                          datos_cuenta =cuenta.split(",")
-#                          Cuenta(datos_cuenta[0],datos_cuenta[1],datos_cuenta[2],datos_cuenta[3])
-
-#                     for deposito in archivo_depositos:
-#                          datos_deposito =deposito.split(",")
-#                          Cuenta.obternerPorCBU(datos_deposito[0]).depositar(int(datos_deposito[1]))
-#                     for retiro in archivo_retiros:
-#                          datos_retiro =retiro.split(",")
-#                          Cuenta.obternerPorCBU(datos_retiro[0]).retirar(int(datos_retiro[1]))
+                         for arquero in archivo_arqueros:
+                              datos_arquero = arquero.split(',')
+                              Arquero(datos_arquero[0],datos_arquero[1],datos_arquero[2],datos_arquero[3],datos_arquero[4],datos_arquero[5],datos_arquero[6],datos_arquero[7],datos_arquero[8],datos_arquero[9],datos_arquero[10],datos_arquero[11],datos_arquero[12],datos_arquero[13],datos_arquero[14])
+                         
+                         for jugadorcampo in archivo_jugadorescampo:
+                              dato_jugadorcampo = jugadorcampo.split(',')
+                              JugadorDeCampo(dato_jugadorcampo[0],dato_jugadorcampo[1],dato_jugadorcampo[2],dato_jugadorcampo[3],dato_jugadorcampo[4],dato_jugadorcampo[5],dato_jugadorcampo[6],dato_jugadorcampo[7],dato_jugadorcampo[8],dato_jugadorcampo[9],dato_jugadorcampo[10],dato_jugadorcampo[11],dato_jugadorcampo[12],dato_jugadorcampo[13],dato_jugadorcampo[14])
