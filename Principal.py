9aa0f521113e7c56d50e01cd9ee8df800d12b8cd
liammacgaw <lmacgaw@itba.edu.ar>
2023-05-04 14:47:03 -0300

    d

--- Principal.py
+++ Principal.py
@@ -146,6 +146,54 @@ def leer_usuarios():
     except:
         print("")
 
+def guardar_archivos():
+    with open('./ligas.txt','w') as archivo_ligas:
+        with open('./clubes.txt','w') as archivo_clubes:
+            with open('./arqueros.txt','w') as archivo_arqueros:
+                with open('./jugadorescampo.txt','w') as archivo_jugadorescampo:
+                    for liga in Liga.lista_ligas:
+                        archivo_ligas.write(f"{liga.nombre},{liga.pais},{liga.lista_clubes},{liga.cant_clubes}\n")
+                    for club in Club.lista_clubes:
+                        archivo_clubes.write(f"{club.nombre},{club.id},{club.liga},{club.presupuesto},{club.valor_del_club},{club.lista_jugadores}\n")
+                    for arquero in Arquero.lista_arqueros:
+                        archivo_arqueros.write(f"{arquero.nombre},{arquero.apellido},{arquero.dni},{arquero.edad},{arquero.nacionalidad},{arquero.estatura},{arquero.peso},{arquero.valor},{arquero.club},{arquero.estado},{arquero.cantidad_partidos},{arquero.cantidad_tarjetas},{arquero.posicion},{arquero.vallas_invictas},{arquero.goles_recibidos}\n")
+                    for jugadorcampo in JugadorDeCampo.lista_jugadorescampo:
+                        archivo_jugadorescampo.writerow(f"{jugadorcampo.nombre},{jugadorcampo.apellido},{jugadorcampo.dni},{jugadorcampo.edad},{jugadorcampo.nacionalidad},{jugadorcampo.estatura},{jugadorcampo.peso},{jugadorcampo.valor},{jugadorcampo.club},{jugadorcampo.estado},{jugadorcampo.cantidad_partidos},{jugadorcampo.cantidad_tarjetas},{jugadorcampo.posicio},{jugadorcampo.goles},{jugadorcampo.asistencias}\n")
+
+def leer_archivo():
+    try: 
+        with open('./ligas.txt','r') as archivo_ligas:
+            with open('./clubes.txt','r') as archivo_clubes:
+                with open('./arqueros.txt','r') as archivo_arqueros:
+                    with open('./jugadorescampo.txt','r') as archivo_jugadorescampo:
+                        for liga in archivo_ligas:
+                            datos_liga = liga.split(',')
+                            datos_liga[3] = datos_liga[3].rstrip("\n")
+                            obj_liga = Liga(datos_liga[0],datos_liga[1],datos_liga[2],datos_liga[3])
+                            Liga.lista_ligas.append(obj_liga)
+                            Liga.lista_nombre_ligas.append(obj_liga.nombre)
+                            Liga.lista_paises_ligas.append(obj_liga.pais)
+                        for club in archivo_clubes:
+                            datos_club = club.split(',')
+                            datos_club[5] = datos_club[5].rstrip("\n")
+                            obj_club = Club(datos_club[0],datos_club[1],datos_club[2],datos_club[3],datos_club[4],datos_club[5])
+                            Club.lista_clubes.append(obj_club)
+                            Club.lista_id_clubes.append(obj_club.id)
+                        for arquero in archivo_arqueros:
+                            datos_arquero = arquero.split(',')
+                            datos_arquero[14] = datos_arquero[14].rstrip("\n")
+                            obj_arquero = Arquero(datos_arquero[0],datos_arquero[1],datos_arquero[2],datos_arquero[3],datos_arquero[4],datos_arquero[5],datos_arquero[6],datos_arquero[7],datos_arquero[8],datos_arquero[9],datos_arquero[10],datos_arquero[11],datos_arquero[12],datos_arquero[13],datos_arquero[14])
+                            Arquero.lista_arqueros.append(obj_arquero)
+                            Persona.lista_dni_personas.append(obj_arquero.dni)
+                        for jugadorcampo in archivo_jugadorescampo:
+                            dato_jugadorcampo = jugadorcampo.split(',')
+                            dato_jugadorcampo[14] = dato_jugadorcampo[14].rstrip("\n")
+                            obj_jugadorcampo = JugadorDeCampo(dato_jugadorcampo[0],dato_jugadorcampo[1],dato_jugadorcampo[2],dato_jugadorcampo[3],dato_jugadorcampo[4],dato_jugadorcampo[5],dato_jugadorcampo[6],dato_jugadorcampo[7],dato_jugadorcampo[8],dato_jugadorcampo[9],dato_jugadorcampo[10],dato_jugadorcampo[11],dato_jugadorcampo[12],dato_jugadorcampo[13],dato_jugadorcampo[14])
+                            Persona.lista_dni_personas.append(obj_jugadorcampo.dni)
+    except:
+        print("")
+
+
 def menu():
     menu=int(input("""Elija una opción del menú (Ingrese el número):
 1- Agregar Liga
@@ -177,8 +225,8 @@ def menu_usuario():
 
 #  revisar los try except
 
-def menu_principal(usu):
-    usu.leer_archivo()
+def menu_principal():
+    leer_archivo()
     while(menu != 10):
         guardo = menu()
         if guardo == 1:
@@ -374,7 +422,7 @@ def menu_principal(usu):
         elif guardo < 1 or guardo >10 :
             print("Error al elegir acción. Intente de nuevo: ")
         elif guardo == 10:
-            usu.guardar_archivos()
+            guardar_archivos()
             break
     # except:
     #     print("Error. Ingrese el numero de la opcion que desea hacer.")
@@ -400,9 +448,8 @@ while(menu_usuario != 5):
                         contrasena = input("Ingrese su contraseña: ")
                         while contrasena != Usuario.lista_usuarios[i].contra:
                             contrasena = input("La contraseña es incorrecta. Intente nuevamente: ")
-                        usu = Usuario.lista_usuarios[i]
                         print("Ingresado correctamente.")
-                        menu_principal(usu)
+                        menu_principal()
                 if esta == "No":
                     print("El nombre de usuario no existe.")
 
